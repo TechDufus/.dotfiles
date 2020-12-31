4f246ffbf464233368d37a5efbf2b4a1759284cb
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2020-12-31 12:58:42 -0600

    Adding PSDepend Check.

--- install.ps1
+++ install.ps1
@@ -2,8 +2,11 @@ $configPath = $PSScriptRoot
 
 # Set up symlinks
 Get-ChildItem -Path $configPath -Directory | ForEach-Object {
-    New-Item -Path ([System.IO.Path]::Combine($HOME, 'config', $_.Name)) -ItemType SymbolicLink -Value $_.FullName
+    New-Item -Path ([System.IO.Path]::Combine($HOME, '.config', $_.Name)) -ItemType SymbolicLink -Value $_.FullName
 }
 
-Install-Module PSDepend -Force
+$IsPSDependInstalled = Get-Module PSDepend -ListAvailable
+If (-Not($IsPSDependInstalled)) {
+    Install-Module PSDepend -Force
+}
 Invoke-PSDepend -Force (Join-Path $PSScriptRoot 'requirements.psd1')
