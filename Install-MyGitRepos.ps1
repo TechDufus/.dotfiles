89da5c5d3c4cec31000fe04fdb99517a85c6c5ac
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2021-01-06 20:41:11 -0600

    Adding Install fonts and regions.

--- Install-MyGitRepos.ps1
+++ Install-MyGitRepos.ps1
@@ -1,3 +1,4 @@
+#Region Variables
 $Repos = @(
     'matthewjdegarmo/Stream',
     'matthewjdegarmo/MyTop8',
@@ -9,11 +10,23 @@ $Repos = @(
     'matthewjdegarmo/PSTeams',
     'powerline/fonts'
 )
+$GitHubPath = 'C:\Projects\GitHub'
+#EndRegion Variables
 
-If (-Not(Test-Path 'C:\Projects\GitHub')) {
-    New-Item -Name GitHub -Path C:\Projects -ItemType Directory
+#Region Git Environment Setup
+If (-Not(Test-Path $GitHubPath)) {
+    New-Item -Name $GitHubPath -ItemType Directory
 }
-Set-Location C:\Projects\GitHub
+Set-Location $GitHubPath
+#EndRegion Git Environment Setup
+
+#Region Clone Git Repos
 $Repos | Foreach-Object -ThrottleLimit $Repos.Count -Parallel {
     git clone https://github.com/$_`.git
 }
+#EndRegion Clone Git Repos
+
+#Region Install all fonts
+Set-Location Join-Path $GitHubPath fonts
+.\install.ps1
+#EndRegion Install all fonts
