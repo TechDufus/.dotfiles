7c785f4aa1552e14ecc3e2ba49ddcc5505614aec
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2021-01-12 22:29:46 -0600

    Syntax and path corrections.

--- .config/PowerShell/install.ps1
+++ .config/PowerShell/install.ps1
@@ -7,14 +7,21 @@ If (Test-Administrator) {
 }
 #EndRegion Install all fonts
 
-If (Test-Path $Profile.CurrentUserAllHosts) {
-    $IsAlreadyReferenced = $profile.CurrentUserAllHosts | Select-String -SimpleMatch '. `"$([System.IO.Path]::Combine($Home, ''.config'', ''PowerShell'', ''))"''profile.ps1''))`"'
+
+$PowerShellProfile = "C:\Users\$env:USERNAME\Documents\PowerShell\profile.ps1"
+
+If (Test-Path $PowerShellProfile) {
+    $ConfigProfilePath = [System.IO.Path]::Combine("$Home", '.config', 'PowerShell', 'profile.ps1')
+    $Pattern = ". `"$ConfigProfilePath`""
+    $IsAlreadyReferenced = Get-Content $PowerShellProfile | Select-String -SimpleMatch $Pattern
     If (-Not($IsAlreadyReferenced)) {
-        Write-Host "Adding path to .config profile to '$($Profile.CurrentUserAllHosts)'"
-        ". $([System.IO.Path]::Combine($Home, '.config', 'PowerShell', 'profile.ps1'))" | Out-File -FilePath $Profile.CurrentUserAllHosts -Append -Force
+        Write-Host "Adding path to .config profile to '$($PowerShellProfile)'"
+        $Pattern | Out-File -FilePath $PowerShellProfile -Append -force -Encoding ascii
     }
 } else {
-    Write-Host "Creating profile: CurrentUserAllHosts: '$($Profile.CurrentUserAllHosts)'"
-    New-Item -Path $Profile.CurrentUserAllHosts -ItemType File -Force
-    ". $([System.IO.Path]::Combine($Home, '.config', 'PowerShell', 'profile.ps1'))" | Out-File -FilePath $Profile.CurrentUserAllHosts -Force
+    Write-Host "Creating profile: CurrentUserAllHosts: '$($PowerShellProfile)'"
+    New-Item -Path $PowerShellProfile -ItemType File -Force
+    $ConfigProfilePath = [System.IO.Path]::Combine($Home, '.config', 'PowerShell', 'profile.ps1')
+    $Pattern = ". `"$ConfigProfilePath`""
+    $Pattern | Out-File -FilePath $PowerShellProfile -Force -Encoding ascii
 }
--- install.ps1
+++ install.ps1
@@ -12,7 +12,7 @@ Get-ChildItem -Path $configPath -Directory | ForEach-Object {
     #! Must be admin
     $ConfigScriptRoot = ([System.IO.Path]::Combine($HOME, '.config', $_.Name))
     New-Item -Path $ConfigScriptRoot -ItemType SymbolicLink -Value $_.FullName -ErrorAction SilentlyContinue
-    . (Join-Path $ConfigScriptRoot 'install.ps1')
+    . ([System.IO.Path]::Combine($ConfigScriptRoot, "$($_.FullName)", 'install.ps1'))
 }
 
 
