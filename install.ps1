2ffdeae6f3c6cf2486be18618de0583c31a9b850
Matthew J. DeGarmo <matthewjdegarmo@gmail.com>
2021-03-25 10:27:16 -0500

    Closes #2- disable symlink creation and update folder references.

--- install.ps1
+++ install.ps1
@@ -8,15 +8,15 @@ $configPath = (Join-Path $PSScriptRoot '.config')
 # Set up symlinks
 Get-ChildItem -Path $configPath -Directory | ForEach-Object {
     #! Must be admin
-    $ConfigScriptRoot = ([System.IO.Path]::Combine($HOME, '.config', $_.Name))
-    New-Item -Path $ConfigScriptRoot -ItemType SymbolicLink -Value $_.FullName -ErrorAction SilentlyContinue
+    # $ConfigScriptRoot = ([System.IO.Path]::Combine($HOME, '.config', $_.Name))
+    # New-Item -Path $ConfigScriptRoot -ItemType SymbolicLink -Value $_.FullName -ErrorAction SilentlyContinue
     If ($_.Name -eq 'PowerShell') {
         $SkipRepoSplat = @{
             SkipPowerShellGitRepos = $SkipPowerShellGitRepos.IsPresent
         }
-        . ([System.IO.Path]::Combine($ConfigScriptRoot, "$($_.FullName)", "install.ps1")) @SkipRepoSplat
+        . ([System.IO.Path]::Combine($configPath, "$($_.FullName)", "install.ps1")) @SkipRepoSplat
     } Else {
-        . ([System.IO.Path]::Combine($ConfigScriptRoot, "$($_.FullName)", 'install.ps1'))
+        . ([System.IO.Path]::Combine($configPath, "$($_.FullName)", 'install.ps1'))
     }
 }
 
