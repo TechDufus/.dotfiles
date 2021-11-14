ce409e6c17fbff8738a98c94aa89204176c714e0
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2021-11-13 18:46:57 -0600

    Adding force to install of script.

--- .config/winfetch/install.ps1
+++ .config/winfetch/install.ps1
@@ -1,8 +1,8 @@
 #Install winfetch script on windows
 if ($IsWindows -or $PSVersionTable.PSVersion.Major -eq 5) {
-    $IsInstalled = Get-InstalledScript 'pwshfetch-test-1'
+    $IsInstalled = Get-InstalledScript 'pwshfetch-test-1' -ErrorAction SilentlyContinue
     If (-Not($IsInstalled)) {
-        Install-Script -Name 'pwshfetch-test-1' -Repository 'PSGallery'
+        Install-Script -Name 'pwshfetch-test-1' -Repository 'PSGallery' -Force
     }
 }
 
