67fc1d5bb03c7c2a6a06c3722f8efa6540d563b3
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2022-09-12 08:39:11 -0500

    all the things

--- nvim/.config/nvim/lua/techdufus/init.lua
+++ nvim/.config/nvim/lua/techdufus/init.lua
@@ -1,42 +1,5 @@
-require("techdufus.set")
-require("techdufus.packer")
-require("techdufus.neogit")
+require('techdufus.options')
+require('techdufus.keymaps')
+require('techdufus.packer')
 
-local augroup = vim.api.nvim_create_augroup
-TechDufusGroup = augroup('TechDufus', {})
 
-local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
-
-function R(name)
-    require("plenary.reload").reload_module(name)
-end
-
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
-})
-
-autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
-    group = TechDufusGroup,
-    pattern = "*.rs",
-    callback = function()
-        require("lsp_extensions").inlay_hints{}
-    end
-})
-
-autocmd({"BufWritePre"}, {
-    group = TechDufusGroup,
-    pattern = "*",
-    command = "%s/\\s\\+$//e",
-})
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
---
+++ nvim/.config/nvim/lua/techdufus/packer.lua
@@ -0,0 +1,9 @@
+return require('packer').startup(function()
+  -- packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- colorscheme
+  use 'gruvbox-community/gruvbox'
+end)
+
+
