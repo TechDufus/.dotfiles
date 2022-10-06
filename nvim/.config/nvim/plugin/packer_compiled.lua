c9044ca33fc3c69261cd506dc279c14a3dd59012
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2022-10-05 22:30:06 -0500

    removing presence

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -167,11 +167,6 @@ _G.packer_plugins = {
     path = "/home/techdufus/.local/share/nvim/site/pack/packer/start/popup.nvim",
     url = "https://github.com/nvim-lua/popup.nvim"
   },
-  ["presence.nvim"] = {
-    loaded = true,
-    path = "/home/techdufus/.local/share/nvim/site/pack/packer/start/presence.nvim",
-    url = "https://github.com/andweeb/presence.nvim"
-  },
   ["telescope-media-files.nvim"] = {
     loaded = true,
     path = "/home/techdufus/.local/share/nvim/site/pack/packer/start/telescope-media-files.nvim",
