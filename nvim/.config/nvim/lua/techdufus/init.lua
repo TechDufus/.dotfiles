a5234faf42b9833157b9c5e6b401b1b9f3d60cbd
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2022-11-10 07:54:12 -0600

    more reorg

--- nvim/.config/nvim/lua/techdufus/init.lua
+++ nvim/.config/nvim/lua/techdufus/init.lua
@@ -2,22 +2,6 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
-
-require('techdufus.plugins.plugins')
-require('techdufus.core.options')
-require('techdufus.terminal')
-require('techdufus.core.keymaps')
-require('techdufus.core.autocommands')
-require('techdufus.plugins.telescope')
-require('techdufus.plugins.autopairs')
-require('techdufus.statusline')
-require('techdufus.nvim-tree')
-require('techdufus.nvim-lightbulb')
-require('techdufus.toggleterm')
-require('techdufus.plugins.lsp.lsp')
-require('techdufus.nvim-go')
-require('techdufus.filetype')
-require("techdufus.color_scheme")
-
-
-
+require('techdufus.core')
+require('techdufus.plugins-setup')
+require('techdufus.plugins')
---
+++ nvim/.config/nvim/lua/techdufus/plugins/lsp/init.lua
@@ -0,0 +1,2 @@
+require('techdufus.plugins.lsp.lsp')
+require('techdufus.plugins.lsp.nvim-go')
